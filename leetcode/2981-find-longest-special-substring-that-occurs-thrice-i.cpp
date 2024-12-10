e445045d95739a6ab73b2882a5dd115b36c4c1a8
kanishk <112634879+KanishkValechha@users.noreply.github.com>
2024-12-11 02:09:08 +0530

    Add LeetCode solution: 2981-find-longest-special-substring-that-occurs-thrice-i.cpp

---
+++ leetcode/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,76 @@
+/*
+ * 2981. Find Longest Special Substring That Occurs Thrice I
+ * Difficulty: Medium
+ * 
+ * You are given a string s that consists of lowercase English letters.
+A string is called
+special
+special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+Return the length of the
+longest special substring
+longest special substring of s which occurs
+at least thrice
+at least thrice, or -1 if no special substring occurs at least thrice.
+A
+substring
+substring is a contiguous
+non-empty
+non-empty sequence of characters within a string.
+
+**Example 1:**
+
+**Example 1:**
+Input: s = "aaaa"
+Output: 2
+
+Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
+It can be shown that the maximum length achievable is 2.
+
+**Example 2:**
+
+**Example 2:**
+Input: s = "abcdef"
+Output: -1
+
+Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
+
+**Example 3:**
+
+**Example 3:**
+Input: s = "abcaba"
+Output: 1
+
+Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
+It can be shown that the maximum length achievable is 1.
+
+**Constraints:**
+
+**Constraints:**
+3 <= s.length <= 50
+s consists of only lowercase English letters.
+ */
+
+        for(auto i: f){
+            if(i.second >= 3){
+        }
+            }
+                }
+                    f[a]++;
+                if(x.size() == 1){
+                }
+                    x.insert(a[l]);
+                for(int l=0; l<a.length(); l++){
+                }
+                set<char> x;
+                    a.push_back(s[k]);
+                string a;
+                for(int k = i; k<=j; k++){
+            for(int j=i; j<s.length(); j++){
+        map<string, int> f;
+        for(int i=0; i<s.length(); i++){
+                int x=i.first.length();
+    int maximumLength(string s) {
+        int ans=-1;
+        int n = s.length();
+class Solution {
+public:
