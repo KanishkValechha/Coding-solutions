ced47a4b3cecee78c4718f3194425a8b2074321c
Kanishk Valechha <112634879+KanishkValechha@users.noreply.github.com>
2025-01-16 15:00:04 +0530

    Add LeetCode solution: 2425-bitwise-xor-of-all-pairings.cpp

---
+++ leetcode/2425-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,60 @@
+/*
+ * 2425. Bitwise XOR of All Pairings
+ * Difficulty: Medium
+ * 
+ * You are given two
+0-indexed arrays, nums1 and nums2, consisting of non-negative integers. There exists another array, nums3, which contains the bitwise XOR of
+all pairings
+all pairings of integers between nums1 and nums2 (every integer in nums1 is paired with every integer in nums2
+exactly once
+exactly once).
+Return the
+bitwise XOR
+bitwise XOR of all integers in nums3.
+
+**Example 1:**
+
+**Example 1:**
+Input: nums1 = [2,1,3], nums2 = [10,2,5,0]
+Output: 13
+
+Explanation:
+A possible nums3 array is [8,0,7,2,11,3,4,1,9,1,6,3].
+The bitwise XOR of all these numbers is 13, so we return 13.
+
+**Example 2:**
+
+**Example 2:**
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 0
+
+Explanation:
+All possible pairs of bitwise XORs are nums1[0] ^ nums2[0], nums1[0] ^ nums2[1], nums1[1] ^ nums2[0],
+and nums1[1] ^ nums2[1].
+Thus, one possible nums3 array is [2,5,1,6].
+2 ^ 5 ^ 1 ^ 6 = 0, so we return 0.
+
+**Constraints:**
+
+**Constraints:**
+1 <= nums1.length, nums2.length <= 105
+0 <= nums1[i], nums2[j] <= 109
+ */
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int t=0;
+        for(int j=0;j<nums2.size();j++){
+            t^=nums2[j];
+        }
+        int ans=0;
+        for(int i=0;i<nums1.size();i++){
+            if(nums2.size()%2==0){
+        }
+        return ans;
+                ans^=t;
+            }
+    }
+            else ans^=t^nums1[i];
+};
